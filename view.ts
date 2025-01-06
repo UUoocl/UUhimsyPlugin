3d55855d598bfb81dc5898d2ab18e9cfbbceb7ae
UUoocl <99063397+UUoocl@users.noreply.github.com>
2025-01-06 16:21:15 -0500

    view cleanup

--- view.ts
+++ view.ts
@@ -21,24 +21,16 @@ export class UUhimsyView extends ItemView{
         container.createEl('h4', { text: 'UUhimsy Tags' });
 
         const files = this.app.vault.getFolderByPath("_slide_Tags").children;
-        console.log(files)
         files.forEach(file => {
-            console.log("basename",file.basename)
-            console.log("files",files)
-            
             let tag = file.basename.split(' - ')
             tag[0] = tag[0].toLowerCase()
             new Setting(container).setName(file.basename)
             .addButton((item) =>{
-                console.log(item)
-                console.log("tag",tag)
-                item.setButtonText("Entrance")
+            item.setButtonText("Entrance")
                 .setCta()
                 .onClick(() => {
                     const lastLeaf = this.app.workspace.getMostRecentLeaf()
-                    console.log("ll",lastLeaf)
                     const lastLeafWorkspace = this.app.workspace.getLeafById(lastLeaf?.id)
-                    console.log(lastLeafWorkspace)
                     
                     this.app.workspace.setActiveLeaf(lastLeafWorkspace,true,true);
                     lastLeaf?.setEphemeralState(lastLeaf?.getEphemeralState())
@@ -48,10 +40,7 @@ export class UUhimsyView extends ItemView{
                     new Notice(`Inserted tag${tag[1]}`)
                     })
                 })
-                .addButton( item => {
-                    //console.log(item)
-                    console.log(file)
-                    
+                .addButton( item => {                
                     item.setButtonText("Exit")
                     .onClick(() => {
                         const lastLeaf = this.app.workspace.getMostRecentLeaf()
@@ -59,19 +48,11 @@ export class UUhimsyView extends ItemView{
                         this.app.workspace.setActiveLeaf(lastLeafWorkspace,true,true);
                         lastLeaf?.setEphemeralState(lastLeaf?.getEphemeralState())
                         this.app.workspace.activeEditor?.editor?.replaceSelection(`<!-- slide data-${tag[0]}-exit="${tag[1]}" -->
-    `)
-                    
+`)              
                         new Notice(`Inserted tag${tag[1]}`)
                     })
                 })
-            //container.createEl('p', {text: file.basename})
-        });
-        // const choices = files.filter((file) => {return file.basename.startsWith("Entrance")})
-        // console.log(choices)
-        // for (const choice in choices) {
-        //     console.log(choice.)
-        //     console.log(`${choice}: ${choice['basename']}`);
-        //   }
+           });
       }
     
       async onClose() {
